0c3fe57e3a34170dcd60aee4b6812e582646ba13
Shinz7777 <78514580+Shinz7777@users.noreply.github.com>
2021-04-01 16:13:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
-const aktif = 'Kapan bot aktif';
+const BotName = 'FZLAGTHBOT'; 
+const instagram = 'agus_alnsyh71- fazaal21_'; 
+const aktif = '06:00-23:30';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Fazal\n' // Nama kamu
+            + 'ORG:FZLAGTHBOT;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6285695157547:+62 856-9515-7547\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
@@ -1451,6 +1451,6 @@ Silakan pilih :
 
 }
 //Hay gay
-//create @agusalnsyh don't delate this please
+//create @fazaal21_ don't delate this please
 	
 })
